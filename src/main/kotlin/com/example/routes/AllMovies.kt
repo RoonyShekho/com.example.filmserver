30e2b8c26ffeae33b359b9022f576faeee093c15
RoonyShekho <roni.sheikho200@gmail.com>
2022-05-06 20:15:55 +0300

    make changes

--- src/main/kotlin/com/example/routes/AllMovies.kt
+++ src/main/kotlin/com/example/routes/AllMovies.kt
@@ -14,8 +14,40 @@ fun Route.getAllMovies(){
     val movieRepository :MovieRepository by inject()
 
 
-    get("/films"){
+
+
+
+    get("/movies"){
         try {
+            val page = call.request.queryParameters["page"]?.toInt()?: 1
+
+            require(page in 1..5)
+
+            val apiResponse = movieRepository.getAllMovies(page)
+
+            call.respond(
+                message = apiResponse,
+                status= HttpStatusCode.OK
+            )
+
+        }catch (e:NumberFormatException){
+            call.respond(
+                message = ApiResponse(
+                    success = false,
+                    message = "Numbers only are allowed"),
+                status = HttpStatusCode.BadRequest
+            )
+        }catch (e:IllegalArgumentException){
+            call.respond(
+                message = ApiResponse(false, "Not found"),
+                status = HttpStatusCode.NotFound
+            )
+        }
+    }
+
+
+    get("/films"){
+     /*   try {
             val page = call.request.queryParameters["page"]?.toInt()?: 1
 
             require(page in 1..5)
@@ -39,6 +71,6 @@ fun Route.getAllMovies(){
                 message = ApiResponse(false, "Not found"),
                 status = HttpStatusCode.NotFound
             )
-        }
+        }*/
     }
 }
