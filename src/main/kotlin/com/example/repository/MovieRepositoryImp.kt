1e08db5da2f2be822f0c012d63c8fd1f111b4663
RoonyShekho <roni.sheikho200@gmail.com>
2022-05-07 14:26:38 +0300

    updated the header and footer

--- src/main/kotlin/com/example/repository/MovieRepositoryImp.kt
+++ src/main/kotlin/com/example/repository/MovieRepositoryImp.kt
@@ -20,17 +20,17 @@ class MovieRepositoryImp:MovieRepository {
             Movie(
                 1,
                 "The Sting",
-                "/images/the_sting",
+                "/images/the_sting.jpeg",
                 "Two grifters team up to pull off the ultimate con.\n",
-                7.6
+                8.3
             ),
 
             Movie(
                 2,
                 "Pulp Fiction",
-                "/images/pulp_fiction",
+                "/images/pulp_fiction.jpeg",
                 "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-                9.0
+                8.9
             )
         )
 
@@ -40,17 +40,17 @@ class MovieRepositoryImp:MovieRepository {
             Movie(
                 3,
                 "Cry Macho",
-                "/images/cry_macho",
+                "/images/cry_macho.jpeg",
                 "A one-time rodeo star and washed-up horse breeder takes a job to bring a man's young son home and away from his alcoholic mom. On their journey, the horseman finds redemption through teaching the boy what it means to be a good man.",
-                6.0
+                5.7
             ),
 
             Movie(
                 4,
                 "The Tragedy of Macbeth",
-                "/images/tragedy_of_macbeth",
+                "/images/the_tragedy_of_macbeth.jpeg",
                 "A Scottish lord becomes convinced by a trio of witches that he will become the next King of Scotland, and his ambitious wife supports him in his plans of seizing power.",
-                9.2
+                7.1
             )
 
         )
@@ -60,18 +60,18 @@ class MovieRepositoryImp:MovieRepository {
         listOf(
             Movie(
                 5,
-                "Clockwork Orange",
-                "/images/a_clockwork_orange",
+                "A Clockwork Orange",
+                "/images/a_clockwork_orange.jpeg",
                 "In the future, a sadistic gang leader is imprisoned and volunteers for a conduct-aversion experiment, but it doesn't go as planned.\n",
-                9.4
+                8.3
             ),
 
             Movie(
                 6,
                 "Vertigo",
-                "/images/vertigo",
+                "/images/vertigo.jpeg",
                 "A former San Francisco police detective juggles wrestling with his personal demons and becoming obsessed with the hauntingly beautiful woman he has been hired to trail, who may be deeply disturbed.",
-                9.0
+                8.3
             )
 
         )
@@ -82,17 +82,17 @@ class MovieRepositoryImp:MovieRepository {
             Movie(
                 7,
                 "Maurice",
-                "/images/clockwork_orange",
+                "/images/maurice.jpeg",
                 "After his lover rejects him, a young man trapped by the oppressiveness of Edwardian society tries to come to terms with and accept his sexuality.\n",
-                9.4
+                7.7
             ),
 
             Movie(
                 8,
-                "Vertigo",
-                "/images/vertigo",
-                "A former San Francisco police detective juggles wrestling with his personal demons and becoming obsessed with the hauntingly beautiful woman he has been hired to trail, who may be deeply disturbed.",
-                9.0
+                "The Deer Hunter",
+                "/images/the_deer_hunter.jpeg",
+                "An in-depth examination of the ways in which the Vietnam War impacts and disrupts the lives of several friends in a small steel mill town in Pennsylvania.",
+                8.1
             )
 
         )
@@ -103,17 +103,17 @@ class MovieRepositoryImp:MovieRepository {
             Movie(
                 9,
                 "Another Country",
-                "/images/another_country",
+                "/images/another_country.jpeg",
                 "Based on the life of the young Guy Burgess, who would become better known as one of the Cambridge Spies.",
-                6.5
+                7.0
             ),
 
             Movie(
                 10,
-                "A Space Odyssey",
-                "/images/a_space_odyssey",
+                "2001: A Space Odyssey",
+                "/images/a_space_odyssey.jpeg",
                 "The Monoliths push humanity to reach for the stars; after their discovery in Africa generations ago, the mysterious objects lead mankind on an awesome journey to Jupiter, with the help of H.A.L. 9000: the world's greatest supercomputer.",
-                10.0
+                8.3
             )
         )
 
@@ -135,7 +135,7 @@ class MovieRepositoryImp:MovieRepository {
         var prevPage:Int? = page
         var nextPage:Int? = page
 
-        if(page in 2..4){
+        if(page in 1..4){
             nextPage = nextPage?.plus(1)
         }
 
